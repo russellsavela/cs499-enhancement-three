44bb7b4343b170255c3831b7b179417380d93620
Russ Savela <russell.savela@snhu.edu>
2025-08-02 13:28:10 -0400

    moved replica

--- terraform/enhancement.three.tf
+++ terraform/enhancement.three.tf
@@ -6,9 +6,6 @@ resource "aws_dynamodb_table" "animals_test" {
   name           = "animals_test"
   stream_view_type = "NEW_AND_OLD_IMAGES"
   stream_enabled   = true
-  replica = {
-    region_name = "us-west-2"
-  }
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "animal_id"
 #  range_key      = ""
@@ -18,6 +15,9 @@ resource "aws_dynamodb_table" "animals_test" {
     type = "S"
   }
 
+  replica = {
+    region_name = "us-west-2"
+  }
 
   ttl {
     attribute_name = "TimeToExist"
